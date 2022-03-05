2cbbba53bc8ecbdeb0bcc17ac0124f823cbb5ab6
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-03-05 19:26:12 +0600

    Update data_delete.c

--- Array/data_delete.c
+++ Array/data_delete.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int student[5] = {1, 2, 5, 8, 10}, size = 5, pos = 3, i;
+
+//working delete:
+for(i = pos-1 ; i < size-1; i++){//2,  i=3
+    student[i] = student[i+1];  //{}
+}
+
+//after deleting the array:
+for(i = 0; i<size-1; i++){
+    printf("%d ", student[i]);
+}
+
+return 0;
+}
 
