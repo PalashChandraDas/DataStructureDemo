457de2936eee56c68ca24d86ddffe3f890984355
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-03-05 19:18:54 +0600

    Update array_traversing.c

--- Array/array_traversing.c
+++ Array/array_traversing.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int LA[] = {2,4,6,8,9};
+    int i, size = 5;
+
+    printf("The array elements are: ");
+
+    for(i = 0; i < size; i++) {
+        printf("%d ", LA[i]); // this is linear array.
+    }
+    printf("\n");
+
+    return 0;
+}
