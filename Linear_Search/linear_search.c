b12c4ab3807bee6f72224ac550296eb9981a32de
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-06-08 17:17:06 +0600

    Update linear_search.c

--- Linear_Search/linear_search.c
+++ Linear_Search/linear_search.c
@@ -2,11 +2,11 @@
 
 int main() {
 
-    int arr[] = {10, 22, 4, 5, 6};//my array
-    int n = sizeof(arr)/sizeof(arr[0]);//length of array
+    int arr[] = {10, 22, 4, 5, 6}; //my array
+    int n = sizeof(arr)/sizeof(arr[0]); //length of array
     int x = 5;//declare searching value
 
-    int result = linear_search(arr, n, x);//calling function
+    int result = linear_search(arr, n, x); //calling function
 
     printf("%d\n", result);
 
