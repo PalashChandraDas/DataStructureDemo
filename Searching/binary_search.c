69e6375606e68780419d1db5a4c6bb139ebeb6f9
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-06-08 17:14:26 +0600

    Create binary_search.c

---
+++ Searching/binary_search.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main() {
+    int arr[] = {2, 4, 5, 8, 11, 12, 14, 15, 16, 20};//ascending order sorted
+    int n = sizeof(arr)/sizeof(arr[0]); //length of array
+    int x = 11;//declaring searching value
+
+    int result = binary_search(arr, n, x);//calling function
+    //printf("%d\n", result);
+
+    if(result == -1) {
+        printf("Not found.\n");
+    } else {
+        printf("Element is found at index: %d\n", result);
+    }
+
+    return 0;
+}
+
+
+int binary_search(int arr[], int n, int x) {
+
+    int left, right, mid;
+
+    left = 0; //first item
+    right = n-1; //last item
+
+    while(left <= right) {
+
+        mid = (left+right)/2;
+
+        if(arr[mid] == x) {
+            return mid;
+        }
+        if(arr[mid] < x) {
+            left = mid+1;
+        } else {
+            right = mid-1;
+        }
+    }
+    return -1;
+}
