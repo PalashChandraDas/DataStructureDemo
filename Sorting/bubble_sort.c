d075b9a8db8e60d0332a605291f33bfc0a95977c
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-06-10 21:48:47 +0600

    Create bubble_sort.c

---
+++ Sorting/bubble_sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+
+    int arr[] = {12, 2, 4, 7, 10, 14};
+    int n = sizeof(arr)/sizeof(arr[0]); //length of array
+    bubble_sort(arr, n); //calling function
+
+    return 0;
+}
+
+void bubble_sort(int arr[], int n) {
+    int i, j, temp;
+
+    for(i = 0; i<n; i++) {
+        for(j = 0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                //swapping
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
