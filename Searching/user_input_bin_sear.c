067a69d89638e53e94e17309143716bcefc41c32
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-06-26 15:38:24 +0600

    Update user_input_bin_sear.c

--- Searching/user_input_bin_sear.c
+++ Searching/user_input_bin_sear.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main(){
+int main() {
 
     int i, n, x;
     printf("Enter the size of array: ");
@@ -7,7 +7,7 @@ int main(){
 
     int arr[n];
     printf("Enter %d numbers in array with ascending order: ", n);
-    for(i = 0; i < n; i++){
+    for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
@@ -17,33 +17,32 @@ int main(){
     int result = binary_search(arr, n, x);//calling function
     //printf("%d\n\n", result);
 
-    if(result == -1){
+    if(result == -1) {
         printf("Not found!!\n");
-    }
-    else{
+    } else {
         printf("Yes, found.\nThe index of number is: %d\n", result);
     }
 
-return 0;
+    
+    return 0;
 }
 
 
-int binary_search(int arr[], int n, int x){
+int binary_search(int arr[], int n, int x) {
 
     int left, right, mid;
     left = 0;
     right = n-1;
 
-    while(left <= right){
+    while(left <= right) {
         //mid = (left+right)/2; //generally use it
         mid = left+(right-left)/2; // To avoid integer overflow
-        if(arr[mid] == x){
+        if(arr[mid] == x) {
             return mid;
         }
-        if(arr[mid] < x){
+        if(arr[mid] < x) {
             left = mid + 1;
-        }
-        else{
+        } else {
             right = mid - 1;
         }
     }
