6121f684bbb528ac9e7ad242f37e323c48e011c2
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-07-28 21:20:04 +0600

    Update circular_queue.c

--- Queue/circular_queue.c
+++ Queue/circular_queue.c
@@ -1,16 +1,18 @@
-#include <stdio.h>
-#define Q_SIZE 3
+#include<stdio.h>
+#define Q_SIZE 5
 
 struct Queue {
     int head, tail;
     int arr[Q_SIZE + 1];
 } q;    //q is Queue type variable
 
+//firstly Head and Tail is zero in this Queue
 void initialize() {
-    q.head = 0;
-    q.tail = 0;
+    q.head = 0;     //called head or front
+    q.tail = 0;     //called tail or rear
 }
 
+//check if the queue is full
 int isFull() {
     if((q.tail + 1) % (Q_SIZE + 1) == q.head)
         return 1;
@@ -18,45 +20,75 @@ int isFull() {
         return 0;
 }
 
+//check if the queue is empty
 int isEmpty() {
-    if(q.head == q.tail)
+    if(q.head == q.tail) //head = 2
         return 1;
     else
         return 0;
 }
 
+//adding an element
 void enqueue(int item) {
     if(isFull())
-        printf("Queue is full..\n");
+        printf("Queue is full...\n");
     else {
         q.arr[q.tail] = item;
-        printf("QUEUE: %d\n", item);
+        printf("Enqueue: %d\n", item);
         q.tail = (q.tail + 1) % (Q_SIZE + 1);
     }
+
 }
 
+//removing an element
 void dequeue() {
     if(isEmpty())
-        printf("Queue is empty.\n");
+        printf("Queue is empty..\n");
     else {
         int item;
         item = q.arr[q.head];
-        printf("\n%d is dequeue from this queue.\n", item); //delete first item
-        q.head = (q.head + 1) % (Q_SIZE + 1);
+        printf("Dequeue %d, from this queue.\n", item);
+        q.head = (q.head + 1) % (Q_SIZE + 1);//2
     }
 }
 
+//display the queue
+void display_queue() {
+    if(isEmpty())
+        printf("Queue is empty...\n");
+    else {
+        int i;
+        printf("\nPrint the full queue:\n");
+        for (i = q.head; i != q.tail; i = (i + 1) % (Q_SIZE + 1)) {
+            printf("%d ", q.arr[i]);
+        }
+    }
 
-
+}
 
 int main() {
-    initialize();
-    enqueue(5);
-    enqueue(10);
-    enqueue(11);
-    enqueue(19);
 
+    initialize();
+
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+    enqueue(60);
+    display_queue();
+
+    printf("\n");
     dequeue();
+    dequeue();
+    display_queue();
+
+    printf("\n");
+    enqueue(60);
+    enqueue(70);
+
+    display_queue();
+    printf("\n");
 
     return 0;
 }
