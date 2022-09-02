088d0dfe623947454629b898cf2d28fe609d1dfc
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-09-02 18:26:15 +0600

    Update bubble_sort.c

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -11,7 +11,7 @@ int main() {
 void bubble_sort(int arr[], int n) {
     int i, j, temp;
 
-    for(i = 0; i < n-1; i++) {
+    for(i = 0; i < n-1; i++) { //steps
         for(j = 0; j < n-i-1; j++) {
             if(arr[j] > arr[j+1]) {
                 //swapping
