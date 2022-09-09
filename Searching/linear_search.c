75864327edc96b786cba3d8e846745b2a57c7c67
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-09-09 19:38:40 +0600

    Update linear_search.c

--- Searching/linear_search.c
+++ Searching/linear_search.c
@@ -1,30 +1,40 @@
 #include<stdio.h>
-
 int main() {
+    int t, size, i, item;
 
-    int arr[] = {10, 22, 4, 5, 6};//my array
-    int n = sizeof(arr)/sizeof(arr[0]);//length of array
-    int x = 5;//declare searching value
+    printf("How many times will you take input: ");
+    scanf("%d", &t);
+    while(t--) {
+        printf("\nEnter the size of array: ");
+        scanf("%d", &size);
 
-    int result = linear_search(arr, n, x);//calling function
+        int arr[size];
+        printf("Please enter %d elements in the array: ");
+        for(i = 0; i < size; i++) {
+            scanf("%d", &arr[i]);
+        }
 
-    printf("%d\n", result);
+        printf("Please enter the searching item: ");
+        scanf("%d", &item);
 
+        int result = linear_search(arr, size, item);
+        if(result == -1) {
+            printf("\nNot found!!\n");
+        } else {
+            printf("\nFound.\nThe position of value is: %d\n", result+1);
+        }
+
+    }
     return 0;
 }
 
-
-int linear_search(int arr[], int n, int x) {
-
+int linear_search(int arr[], int size, int item) {
     int i;
-
-    for(i = 0; i<n; i++) {
-        if(arr[i] == x) {
-            return i; //index
+    for(i = 0; i < size; i++) {
+        if(arr[i] == item) {
+            return i;
         }
     }
-
     i = -1;
-    return i; //out of array
-
+    return i;
 }
