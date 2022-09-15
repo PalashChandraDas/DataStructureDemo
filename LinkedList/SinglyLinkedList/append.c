2154199d30488c22f50efff8273767a7287987a8
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-09-15 21:39:50 +0600

    Update append.c

--- LinkedList/SinglyLinkedList/append.c
+++ LinkedList/SinglyLinkedList/append.c
@@ -3,37 +3,37 @@ typedef struct node Node; //struct node = Node
 struct node {
     int data; //data part of node
     Node *next; //address part of node
-} *st_node; //name of the structure type variable
+} *head; //name of the structure type variable
 
 //create node
 void createNodeList(int n) {
-    Node *current_node, *fn_node;
+    Node *current_node, *new_node;
     int num, i;
-    st_node = (Node *)malloc(sizeof(Node)); //memory allocate for first node
+    head = (Node *)malloc(sizeof(Node)); //memory allocate for first node
 
-    if(st_node == NULL) {
+    if(head == NULL) {
         printf("Error! Couldn't create a new node.\n");
         exit(1);
     } else {
         printf("Input the data of node 1: ");
         scanf("%d", &num);
-        st_node->data = num;
-        st_node->next = NULL;
-        current_node = st_node; //pass the address of first node in current_node
+        head->data = num;
+        head->next = NULL;
+        current_node = head; //pass the address of first node in current_node
 
         //create 2nd to n node
         for(i = 2; i <= n; i++) {
             int num;
-            fn_node = (Node *)malloc(sizeof(Node)); //memory allocate for next node
-            if(fn_node == NULL) {
+            new_node = (Node *)malloc(sizeof(Node)); //memory allocate for next node
+            if(new_node == NULL) {
                 printf("Error! Couldn't create a node.\n");
                 break;
             } else {
                 printf("Enter the data of node %d: ", i);
                 scanf("%d", &num);
-                fn_node->data = num;
-                fn_node->next = NULL;
-                current_node->next = fn_node; //links previous node
+                new_node->data = num;
+                new_node->next = NULL;
+                current_node->next = new_node; //links previous node
                 current_node = current_node->next;
             }
         }
@@ -42,30 +42,30 @@ void createNodeList(int n) {
 
 //insert a new node at the end of Linked List
 void append(int num) {
-    Node *current_node, *fn_node;
-    fn_node = (Node *)malloc(sizeof(Node)); //memory allocate for new node
+    Node *current_node, *new_node;
+    new_node = (Node *)malloc(sizeof(Node)); //memory allocate for new node
 
-    if(fn_node == NULL) {
+    if(new_node == NULL) {
         printf("Error! Couldn't create a new node. Because, memory can not be allocated.\n");
         exit(1);
     } else {
-        current_node = st_node;
+        current_node = head;
         while(current_node->next != NULL) {
             current_node = current_node->next;
         }
-        current_node->next = fn_node; //Links the address part
-        fn_node->data = num; //Links the data part
-        fn_node->next = NULL;
+        current_node->next = new_node; //Links the address part
+        new_node->data = num; //Links the data part
+        new_node->next = NULL;
     }
 }
 
 //display list
 void print_linked_list() {
     Node *current_node;
-    if(st_node == NULL) {
+    if(head == NULL) {
         printf("No data found in the empty list.\n");
     } else {
-        current_node = st_node;
+        current_node = head;
         while(current_node != NULL) {
             printf("Data = %d\n", current_node->data); // prints the data of current node
             current_node = current_node->next; // advances the position of current node
