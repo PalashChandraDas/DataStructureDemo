c09fb35cde7ca777549c3dcee3d15bc0ffb19bc0
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-09-15 19:05:40 +0600

    Update create and display Singly Linked List.c

--- LinkedList/SinglyLinkedList/create and display Singly Linked List.c
+++ LinkedList/SinglyLinkedList/create and display Singly Linked List.c
@@ -1,26 +1,28 @@
 #include<stdio.h>
-typedef struct node Node;
+typedef struct node Node; //struct node = Node
 struct node {
-    int data;
-    Node *next;
+    int data; //data part of node
+    Node *next; //address part of node
 } *st_node; //name of the structure type variable
 
+//create node
 void createNodeList(int n) {
     Node *current_node, *fn_node;
     int num, i;
     st_node = (Node *)malloc(sizeof(Node)); //memory allocate for first node
-    printf("Input the data of node 1: ");
-    scanf("%d", &num);
 
     if(st_node == NULL) {
         printf("Error! Couldn't create a new node.\n");
         exit(1);
     } else {
+        printf("Input the data of node 1: ");
+        scanf("%d", &num);
         st_node->data = num;
         st_node->next = NULL;
         current_node = st_node; //pass the address of first node in current_node
 
-        for(i = 2; i <= n; i++) { //create 2nd to n node
+        //create 2nd to n node
+        for(i = 2; i <= n; i++) {
             int num;
             fn_node = (Node *)malloc(sizeof(Node)); //memory allocate for next node
             if(fn_node == NULL) {
@@ -31,20 +33,21 @@ void createNodeList(int n) {
                 scanf("%d", &num);
                 fn_node->data = num;
                 fn_node->next = NULL;
-                current_node->next = fn_node;
-                current_node = current_node->next; //pass the address of next node
+                current_node->next = fn_node; //links previous node
+                current_node = current_node->next;
             }
 
         }
     }
 }
 
+//display list
 void print_linked_list() {
     Node *current_node;
     current_node = st_node;
     while(current_node != NULL) {
-        printf("Data = %d\n", current_node->data);
-        current_node = current_node->next;
+        printf("Data = %d\n", current_node->data); //print the data of current node
+        current_node = current_node->next; //advances the position of current node
     }
 }
 
@@ -53,7 +56,7 @@ int main() {
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
     createNodeList(n);
-    printf("\n");
+    printf("\nLinked list are:\n");
     print_linked_list();
 
     return 0;
